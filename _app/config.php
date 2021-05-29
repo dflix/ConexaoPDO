4e2dcd2ecff3672f5a06074a598ce58616e30512
Marcio Leite <mbleusou@gmail.com>
2021-05-29 16:50:21 -0300

    Conexão mysql PDO

---
+++ _app/config.php
@@ -0,0 +1,12 @@
+<?php
+
+$host = "localhost";
+$dbUser = "root";
+$dbPass = "pass";
+$dbName = "nome_banco";
+
+define("CONF_DB_HOST", $host);
+define("CONF_DB_USER", $dbUser);
+define("CONF_DB_PASS", $dbPass);
+define("CONF_DB_NAME", $dbName);
+
---
+++ test/index.php
@@ -0,0 +1,8 @@
+<?php
+
+require __DIR__ . "/../lib_ext/autoload.php";
+require __DIR__ . "/../_app/config.php";
+
+$conn = new \Source\Conn();
+
+$conn->getConn();
